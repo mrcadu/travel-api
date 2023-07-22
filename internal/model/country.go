a1906ab848967584121374966f654c6a0629001f
Carlos Eduardo <carlosvicc14@gmail.com>
2023-07-22 20:08:35 +0100

    Adding checklist to Country Model

--- internal/model/country.go
+++ internal/model/country.go
@@ -1,7 +1,8 @@
 package model
 
 type Country struct {
-	Id         string `bson:"_id" json:"id"`
-	Name       string `bson:"name" json:"name"`
-	PictureUrl string `bson:"pictureUrl" json:"pictureUrl"`
+	Id         string   `bson:"_id" json:"id"`
+	Name       string   `bson:"name" json:"name"`
+	PictureUrl string   `bson:"pictureUrl" json:"pictureUrl"`
+	Checklist  []string `bson:"checklist" json:"checklist"`
 }
