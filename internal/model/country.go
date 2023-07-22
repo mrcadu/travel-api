b04bb1a26c226eb73e0ba8f78d2738722b177850
Carlos Eduardo <carlosvicc14@gmail.com>
2023-07-22 20:30:54 +0100

    Adding initial and final travel date

--- internal/model/country.go
+++ internal/model/country.go
@@ -1,8 +1,12 @@
 package model
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type Country struct {
-	Id         string   `bson:"_id" json:"id"`
-	Name       string   `bson:"name" json:"name"`
-	PictureUrl string   `bson:"pictureUrl" json:"pictureUrl"`
-	Checklist  []string `bson:"checklist" json:"checklist"`
+	Id          string             `bson:"_id" json:"id"`
+	Name        string             `bson:"name" json:"name"`
+	PictureUrl  string             `bson:"pictureUrl" json:"pictureUrl"`
+	Checklist   []string           `bson:"checklist" json:"checklist"`
+	InitialDate primitive.DateTime `bson:"initialDate" json:"initialDate"`
+	FinalDate   primitive.DateTime `bson:"finalDate" json:"finalDate"`
 }
